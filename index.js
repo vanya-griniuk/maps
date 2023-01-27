312e39c0fad55814c4ba151053ee08061ebda6ca
vanya-griniuk <vanyagriniuk@gmail.com>
2023-01-27 14:48:09 +0200

    improved the edit toolbar

--- index.js
+++ index.js
@@ -10,7 +10,50 @@ map.addLayer(drawnItems);
 
 var drawControl = new L.Control.Draw({
   edit: {
-    featureGroup: drawnItems
-  }
+    featureGroup: drawnItems,
+  },
+  draw: {
+    polygon: {
+      shapeOptions: {
+        color: 'purple'
+      }
+    },
+    polyline: {
+      shapeOptions: {
+        color: 'red'
+      },
+    },
+    rect: {
+      shapeOptions: {
+        color: 'green'
+      },
+    },
+    circle: {
+      shapeOptions: {
+        color: 'purple'
+      },
+    },
+  },
+
 });
 map.addControl(drawControl);
+
+map.on("draw:created", function (e) {
+  var type = e.layerType;
+  var layer = e.layer;
+  console.log(layer.toGeoJSON())
+
+  layer.bindPopup(`<p>${JSON.stringify(layer.toGeoJSON())}</p>`)
+  drawnItems.addLayer(layer);
+});
+
+map.on("draw:edited", function (e) {
+  var layers = e.layers;
+  var type = e.layerType;
+
+  layers.eachLayer(function (layer) {
+    console.log(layer)
+  })
+
+});
+
